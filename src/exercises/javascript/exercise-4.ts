2391dd7e3e137003efea7d49292b683ec9c5476e
Arun <arun.yogeshwaran@zenjob.com>
2024-09-10 13:11:36 +0200

    Refactor exercise4 function to group elements by data type

--- src/exercises/javascript/exercise-4.ts
+++ src/exercises/javascript/exercise-4.ts
@@ -1,4 +1,4 @@
-/**
+ /**
  * Create a function that takes an array of mixed data types and returns an object where the keys
  * are the data types (e.g., 'number', 'string', etc.), and the values are arrays of elements
  * from the input array that match each data type.
@@ -8,7 +8,18 @@
  * Input: [1, "2", 3, "true", false, 42, "hello"]
  * Output: {number: [1, 3, 42], string: ["2", "true", "hello"], boolean: [false]}
  */
-export const exercise4 = (input: any[]): Partial<{ [K in TKey]: any[] }> => {};
+export const exercise4 = (input: any[]): Partial<{ [K in TKey]: any[] }> => {
+    return input.reduce((acc, curr) => {
+        const type = typeof curr;
+        if (acc[type]) {
+            acc[type].push(curr);
+        } else {
+            acc[type] = [curr];
+        }
+
+        return acc;
+    }, {});
+};
 
 const type = typeof ({} as any);
 type TKey = typeof type;
