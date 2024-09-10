566269b62b1603734251e9fca324cb1a320a7fac
Arun <arun.yogeshwaran@zenjob.com>
2024-09-10 17:52:30 +0200

    Refactor exercise11 function to calculate the difference in days between two dates

--- src/exercises/javascript/exercise-11.ts
+++ src/exercises/javascript/exercise-11.ts
@@ -5,4 +5,6 @@
  * Input: "2024-08-14", "2024-08-20"
  * Output: 6
  */
-export const exercise11 = (date1: string, date2: string): number => {};
+export const exercise11 = (date1: string, date2: string): number => {
+    return Math.abs(new Date(date1).getTime() - new Date(date2).getTime()) / (1000 * 60 * 60 * 24);
+};
