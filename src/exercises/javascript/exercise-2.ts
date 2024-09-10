0068de439befce718567e830ef9f4da8ac623c70
Arun <arun.yogeshwaran@zenjob.com>
2024-09-10 11:50:46 +0200

    Refactor exercise2 function to sort input number in descending order

--- src/exercises/javascript/exercise-2.ts
+++ src/exercises/javascript/exercise-2.ts
@@ -8,4 +8,8 @@
  * Input: 145263
  * Output: 654321
  */
-export const exercise2 = (input: number): number => {};
+export const exercise2 = (input: number): number => {
+    let sortedNumbers = input.toString().split("").sort((a, b) => parseInt(b) - parseInt(a));
+    let numString = sortedNumbers.join("");
+    return parseInt(numString);
+};
